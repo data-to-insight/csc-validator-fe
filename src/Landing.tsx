d552cf071b190ff6361d4cfe9e35f5a0dd36ba6a
Tambe Tabitha <tambe.tabitha@socialfinance.org.uk>
2023-12-04 17:30:26 +0000

    restore cin tool as default

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -40,7 +40,7 @@ const Landing = (props: LandingProps) => {
           sourceLink='https://github.com/data-to-insight/cin-validator-fe'
         />
         <Version versionNumber='0.1.3' sourceLink='https://github.com/data-to-insight/csc-validator-be-cin'/>
-        <Version versionNumber='0.1.0' sourceLink='https://github.com/data-to-insight/csc-validator-be-903'/>
+        <Version versionNumber='0.1.1' sourceLink='https://github.com/data-to-insight/csc-validator-be-903'/>
       </Container>
     </ThemeProvider>
   );
